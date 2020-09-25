7a54493839872d474de79d693b8b2e92a3b9fce7
980765465@qq.com <lkd980765465>
2020-09-25 11:15:35 +0800

    twelve commit 'modify layout for this project & made it works'

--- antd-react-ts/src/pages/home/index.tsx
+++ antd-react-ts/src/pages/home/index.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import { Button } from 'antd';
 import { Link } from "react-router-dom";
+import { createSelector } from "reselect";
+import { useSelector, useDispatch } from "react-redux";
+import {RootState} from "@/stores/reducers";
+import { doChangeName } from "../../stores/actions";
 
 export default function () {
+  const mapState = createSelector(
+    (state: RootState) => state,
+    ({app: {name, age}}) => ({name,age})
+  );
+  const { name, age } = useSelector(mapState);
+  const dispatch = useDispatch();
   return (
     <div>
       <Button
@@ -12,6 +22,12 @@ export default function () {
         to about
       </Button>
       <Link to='/about'>to about</Link>
+      <p>my name is {name}</p>
+      <Button
+        type='primary'
+        onClick={()=>{dispatch(doChangeName('kakakaka'))}}
+      >
+        change name</Button>
     </div>
   );
 }
